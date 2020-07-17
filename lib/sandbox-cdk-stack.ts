00588acbc7bcabac0bc4945eb1e34571225afe03
moaible <motodera@moapp.info>
2020-07-17 13:26:48 +0000

    Add lambda stack

--- lib/sandbox-cdk-stack.ts
+++ lib/sandbox-cdk-stack.ts
@@ -1,9 +1,14 @@
 import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda';
 
 export class SandboxCdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-    // The code that defines your stack goes here
+    // defines an AWS Lambda resource
+    const hello = new lambda.Function(this, 'HelloWorldHandler', {
+      runtime: lambda.Runtime.NODEJS_10_X,
+      code: lambda.Code.asset('src/'),
+      handler: 'lambda/helloworld'
+    });
   }
 }
